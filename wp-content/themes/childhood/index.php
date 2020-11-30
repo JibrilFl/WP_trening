2eb0600dba9c211f85fe963f9545a4e6920ed94c
JibrilFl <jibrilfl@gmail.com>
2020-11-30 19:04:05 +0500

    hook

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -502,7 +502,37 @@ get_header();
                 </div>
             </div>
         </div>
+
+<?php
+    // Хук события
+    // function print_hello($text, $name) {
+    //     echo 'Hello world' . $text . '' . $name;
+    // };
+
+    // add_action('my_hook', 'print_hello', 10, 2);
+
+    // do_action( 'my_hook', 'dear customer', 'Sashka' );
+
+    // Хук фильтра
+    // function my_filter_function($str) {
+    //     return 'Hello' . $str . '<br>';
+    // };
+
+    // add_filter('my_filter', 'my_filter_function', 15);
+    
+    // echo apply_filters( 'my_filter', 'World' );
+
+    // remove_filter('my_filter', 'my_filter_function', 15);
+
+    // echo apply_filters( 'my_filter', 'World' ); Избавляемся от хука фильтра
+
+    // remove_action ----- Так же избавляемся от хука но только у события
+
+    // Следует разделять хуки событий и фильтров
+?>
 <?php
     get_footer();
 ?>
+
+
         
